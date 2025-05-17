38ea80a82daf95d4d912080e48cd8e93d07a9fd8
Dimas Fadilah Akbar <dimasfadilah20@gmail.com>
2025-05-17 22:53:56 +0700

    feat: add middleware cors

--- src/middleware/acceskey.go
+++ src/middleware/acceskey.go
@@ -14,6 +14,17 @@ import (
 // AccessKeyMiddleware validates the Access-Key in the request header
 func AccessKeyMiddleware(security *helpers.Security, secretKey string, expireSeconds int64) gin.HandlerFunc {
 	return func(c *gin.Context) {
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST, HEAD, PATCH, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == http.MethodOptions {
+			c.AbortWithStatus(204)
+			return
+		}
+
 		// Ambil Access-Key dari header
 		accessKey := c.GetHeader("Access-Key")
 		if accessKey == "" {
